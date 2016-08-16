a7b9803186b95dc8f45b5701e8a560b1a2080ff8
BrezhnevSergey <bsa_84@ukr.net>
2016-08-17 01:06:11 +0300

    +ADD tsk 5-6

--- 5.FunctionsClosures/js/code.js
+++ 5.FunctionsClosures/js/code.js
@@ -87,3 +87,105 @@ users.forEach(function(user) {
   console.log( user.age );
 })*/
 
+//task5
+/*var arr = [1, 2, 3, 4, 5, 6, 7];
+
+function filter(arr, func) {
+  var newArray = [];
+  arr.forEach (function(item, i, arr) {
+    if(func(item)) {
+      newArray.push(item);
+    }
+  });
+  return newArray;
+  filter.inBetween = function(a, b) {
+    if(a <= item <= b){
+      return true;
+    }
+  }
+}
+
+function inBetween(a, b) {
+  return function(x) {
+    return x >= a && x <= b;
+  }
+}
+
+function inArray(array) {
+  return function(x) {
+//    for(var i = 0; i < array.length; i++){
+//      if(x == array[i])
+//        return true;
+//    }
+    //or
+     return array.indexOf(x) != -1;
+  }
+}
+
+
+console.log(filter(arr, function(a) {
+  return a % 2 == 0;
+}));
+console.log(filter(arr, inBetween(3, 6)));
+console.log(filter(arr, inArray([1, 2, 10])));*/
+
+//task6
+//1
+/*function makeArmy() {
+
+  var shooters = [];
+
+  for (var i = 0; i < 10; i++) {
+    var shooter = function me() { // функция-стрелок
+      console.log( me.i ); // выводит свой номер
+    };
+    shooter.i = i;
+    shooters.push(shooter);
+  }
+
+  return shooters;
+}
+*/
+//2
+/*function makeArmy() {
+
+  var shooters = [];
+
+  for (var i = 0; i < 10; i++) {
+
+    var shooter = (function(x) {
+
+      return function() {
+        console.log( x );
+      };
+
+    })(i);
+
+    shooters.push(shooter);
+  }
+
+  return shooters;
+}*/
+//3
+function makeArmy() {
+
+  var shooters = [];
+
+  for (var i = 0; i < 10; i++)(function(i) {
+
+    var shooter = function() {
+      console.log( i );
+    };
+
+    shooters.push(shooter);
+
+  })(i);
+
+  return shooters;
+}
+
+var army = makeArmy();
+
+army[0](); // стрелок выводит 10, а должен 0
+army[5](); // стрелок выводит 10...
+// .. все стрелки выводят 10 вместо 0,1,2...9
