e3f909701debcb1652779912c539a1d2dd855397
BrezhnevSergey <bsa_84@ukr.net>
2016-08-24 23:18:33 +0300

    +change DOM4

---
+++ 2. Document & page object/4. DOM traversing/js/jscript.js
@@ -0,0 +1,24 @@
+//task1
+  	//	console.log(document.documentElement.children[0]);
+   	//	console.log(document.documentElement.firstChild);
+    //  console.log(document.head);
+   	//	console.log(document.body.children[1]);
+   	//	console.log(document.body.children[1].children[1]);
+
+
+//task2
+    /*var element = document.body.children[1].children[1];
+    console.log(element.childNodes);
+    console.log(element.childNodes.length);
+   	if (!element.childNodes.length) 
+		console.log("Emty element");
+   	//or
+   	//if (!element.firstChild) 
+   	//	console.log("Emty element");
+   	//or
+   	//if (!element.lastChild) 
+   	//	console.log("Emty element");*/
+    //or
+    //console.log(element.hasChildNodes());
+    //if ( !element.hasChildNodes() ) 
+    //    console.log("Emty element");*/
